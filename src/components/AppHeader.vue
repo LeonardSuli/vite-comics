12a132e52140f72c6f85661cb9382e13748a7af0
LeonardSuli <leonardsuli1991@gmail.com>
2024-02-03 01:56:24 +0100

    add header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,14 +8,66 @@ export default{
 
   <header>
 
+    <div class="container">
+
+      <div class="logo">
+        <img src="/img/dc-logo.png">
+      </div>
+  
+      <nav>
+        <ul class="main_menu">
+          <li><a href="#">CHARACTERS</a></li>
+          <li><a href="#" class="active">COMICS</a></li>
+          <li><a href="#">MOVIES</a></li>
+          <li><a href="#">TV</a></li>
+          <li><a href="#">GAMES</a></li>
+          <li><a href="#">COLLECTIBLES</a></li>
+          <li><a href="#">VIDEOS</a></li>
+          <li><a href="#">FANS</a></li>
+          <li><a href="#">NEWS</a></li>
+          <li><a href="#">SHOP</a></li>
+        </ul>
+      </nav>
+
+    </div>
+
   </header>
-
-
-
 
 </template>
 
 <style scoped>
+
+header{
+
+}
+
+.container{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 0;
+}
+
+.logo img{
+  width: 60px;
+}
+
+.main_menu{
+  list-style: none;
+  display: flex;
+}
+
+.main_menu a{
+  text-decoration: none;
+  padding: 2.15rem 0.75rem;
+  color: var(--dc-secondary);
+  font-size: 10px;
+  font-weight: 700;
+}
+.main_menu a.active{
+  border-bottom: 5px solid var(--dc-primary);
+  color: var(--dc-primary);
+}
 
 
 </style>
