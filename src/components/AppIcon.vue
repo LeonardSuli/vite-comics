951ca12ddf1610357f312cfa75bc61dd34216c37
LeonardSuli <leonardsuli1991@gmail.com>
2024-02-03 17:14:00 +0100

    add AppIcon component

---
+++ src/components/AppIcon.vue
@@ -0,0 +1,76 @@
+<script>
+export default{
+  name: 'AppIcon'
+}
+</script>
+
+
+<template>
+  
+    <div class="main_bottom">
+
+      <div class="container">
+        
+        <div class="icons d-flex">
+
+          <div class="icon">
+            <img src="/img/buy-comics-digital-comics.png" alt="">
+            <span>DIGITAL COMICS</span>
+          </div>
+
+          <div class="icon">
+            <img src="/img/buy-comics-merchandise.png" alt="">
+            <span>DC MERCHANDISE</span>
+          </div>
+
+          <div class="icon">
+            <img src="/img/buy-comics-subscriptions.png" alt="">
+            <span>SUBSCRIPTIONS</span>
+          </div>
+
+          <div class="icon">
+            <img src="/img/buy-comics-shop-locator.png" alt="">
+            <span>COMIC SHOP LOCATOR</span>
+          </div>
+
+          <div class="icon">
+            <img src="/img/buy-dc-power-visa.svg" alt="">
+            <span>DC POWER VISA</span>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+</template>
+
+
+<style scoped>
+
+.main_bottom{
+  background-color: var(--dc-primary);
+  color: var(--dc-light);
+  padding: 2rem 0;
+}
+
+.icons{
+  justify-content: space-around;
+}
+
+.icon{
+  display: flex;
+  align-items: center;
+}
+
+.icon img{
+  width: 35px;
+}
+
+.icon span{
+  padding-left: 1rem;
+  font-size: 10px;
+}
+
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,12 @@
 <script>
+
+import AppIcon from './AppIcon.vue';
+
 export default{
-  name: 'AppMain'
+  name: 'AppMain',
+  components: {
+    AppIcon
+  }
 }
 </script>
 
@@ -16,43 +22,8 @@ export default{
         </div>
 
       </div>
-  
-      <div class="main_bottom">
 
-        <div class="container">
-          
-          <div class="cards d-flex">
-
-            <div class="card">
-              <img src="/img/buy-comics-digital-comics.png" alt="">
-              <span>DIGITAL COMICS</span>
-            </div>
-
-            <div class="card">
-              <img src="/img/buy-comics-merchandise.png" alt="">
-              <span>DC MERCHANDISE</span>
-            </div>
-
-            <div class="card">
-              <img src="/img/buy-comics-subscriptions.png" alt="">
-              <span>SUBSCRIPTIONS</span>
-            </div>
-
-            <div class="card">
-              <img src="/img/buy-comics-shop-locator.png" alt="">
-              <span>COMIC SHOP LOCATOR</span>
-            </div>
-
-            <div class="card">
-              <img src="/img/buy-dc-power-visa.svg" alt="">
-              <span>DC POWER VISA</span>
-            </div>
-
-          </div>
-
-        </div>
-
-      </div>
+      <AppIcon></AppIcon>
 
     </main>
 
@@ -65,30 +36,6 @@ export default{
   background-color: var(--dc-dark);
   color: var(--dc-light);
   padding: 2rem 0;
-}
-
-.main_bottom{
-  background-color: var(--dc-primary);
-  color: var(--dc-light);
-  padding: 2rem 0;
-}
-
-.cards{
-  justify-content: space-around;
-}
-
-.card{
-  display: flex;
-  align-items: center;
-}
-
-.card img{
-  width: 35px;
-}
-
-.card span{
-  padding-left: 1rem;
-  font-size: 10px;
 }
 
 </style>
